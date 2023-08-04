12fb081a5400027a2a66957d9b867cde6c943c88
edbecnel <edbecnel@gmail.com>
2023-08-04 17:25:25 +0800

    Sort the games

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -11,11 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card key={game.id} width="300px" borderRadius={10} overflow="hidden">
-      <Image
-        src={
-          game.background_image ? getCroppedImageUrl(game.background_image) : ""
-        }
-      ></Image>
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
